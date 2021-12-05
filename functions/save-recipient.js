9198814d09d36bbb4b408a3c8bb938c648dc9fe9
Sten Muchow <sten.muchow@gmail.com>
2021-12-05 15:49:46 -0500

    wip

--- functions/save-recipient.js
+++ functions/save-recipient.js
@@ -1,13 +1,9 @@
-
-
-exports.handler = (event, context, callback) => {
-
+exports.handler = async (event, context, callback) => {
   const data = JSON.parse(event.body);
   console.log("Function `save recipient` invoked", data);
-  
+
   return {
     statusCode: 200,
     body: JSON.stringify(data),
   };
-  
 };
